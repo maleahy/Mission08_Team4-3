db7e279d86374a50e1ff8fa3185b8db3a45cb89e
suyeol <suyeol@SUYEOL5544>
2024-02-27 23:41:06 -0700

    Create context stuff

---
+++ Mission08_Team4-3/Models/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team4_3.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ Mission08_Team4-3/Models/TaskContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Team4_3.Models
+{
+    public class TaskContext: DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CreateTasks> Movies { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>().HasData(
+
+                new Categories { CategoryId = 1, Category = "Home" },
+                new Categories { CategoryId = 2, Category = "School" },
+                new Categories { CategoryId = 3, Category = "Work" },
+                new Categories { CategoryId = 4, Category = "Church" }
+                );
+        }
+    }
+}
