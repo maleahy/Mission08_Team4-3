75f36b3cfedc90dd15710ffcf908516d53e399af
suyeol <suyeol@SUYEOL5544>
2024-02-28 14:53:26 -0700

    changes

--- Mission08_Team4-3/Controllers/HomeController.cs
+++ Mission08_Team4-3/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace Mission08_Team4_3.Controllers
             return View();
         }
         // Render the Create View
+        [HttpGet]
         public IActionResult Create()
         {
             // Create a viewbag to create a dropdown option ***Need to set up Program.cs to make this working -Su***
@@ -28,8 +29,34 @@ namespace Mission08_Team4_3.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(CreateTasks response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(response);
+                _context.SaveChanges();
+
+                // Render the Confirmation View
+                return View("Confirmation", response);
+            }
+            else
+            {
+                ViewBag.CategoryTable = _context.Categories
+                    .OrderBy(x => x.Category)
+                    .ToList();
+
+                return View(response);
+            }
+        }
+
         // Render the Quadrants View
         public IActionResult Quadrants()
+        {
+            return View();
+        }
+        public IActionResult Confirmation()
         {
             return View();
         }
